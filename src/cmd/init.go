8294c9072718cfc9c267599fd576026349391a26
HerbertHe <Herbert.He0229@gmail.com>
2020-03-14 14:35:03 +0800

    更新使用帮助

--- src/cmd/init.go
+++ src/cmd/init.go
@@ -1,15 +1,16 @@
 package cmd
 
 import (
-	"cobra.new/utils"
 	"fmt"
+
+	"cobra.new/utils"
 	"github.com/spf13/cobra"
 )
 
 var (
-	remote string
+	remote   string
 	projName string
-	temp string
+	temp     string
 )
 
 // initCmd represents the init command
@@ -19,9 +20,8 @@ var initCmd = &cobra.Command{
 	Long: `
 从远程获取项目模板
 edever init -r [github/gitee] 从Gitee获取(默认从Gitee)
-edever init -s 选择含有sqlite3的模板(默认不含)
 edever init -d 自定义clone的项目名称(默认为仓库名称)
-edever init -r gitee -d test -s 生成目录为test的不含sqlite3的工程
+edever init --temp [sample/sqlite3/ts] 获取指定版本的模板(默认为sample)
 `,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("正在从远端拉取模板...")
@@ -55,7 +55,6 @@ edever init -r gitee -d test -s 生成目录为test的不含sqlite3的工程
 func init() {
 	rootCmd.AddCommand(initCmd)
 	initCmd.Flags().StringVarP(&remote, "repo", "r", "gitee", "edever init -r [github/gitee]")
-	//initCmd.Flags().BoolVarP(&sqlite, "sqlite3", "s", false, "edever init -s [true/false]")
 	initCmd.Flags().StringVar(&temp, "temp", "sample", "edever init --temp[sample/sqlite3/ts]")
 	initCmd.Flags().StringVarP(&projName, "dirname", "d", "ElectronProjectTemplate", "edever init -d (dirname)")
 }
